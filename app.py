4f3649c6f13a24ba0019c054fde7c7bbda7212ae
Preethi Narayan <preeths.narayan@gmail.com>
2021-07-15 14:23:44 -0700

    Init commit
    Adding app.py, Dockerfile and requirements.txt

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import json
+from flask.wrappers import Response
+import logging
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    app.logger.info('Main request successful!')
+    return "Hello World!"
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK-Healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Status request successful!')
+    return response
+
+@app.route("/metrics")
+def metrics():
+    data = {
+            'UserCount': 140,
+            'UserCountActive': 23
+            }
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics request successful!')
+    return response
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+    app.run(host='0.0.0.0')
